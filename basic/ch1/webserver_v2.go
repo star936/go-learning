04061d50a506e6c65083cfe0ce583f805f5e08a8
HaiYang Dang <1542938982@qq.com>
2019-10-20 15:47:21 +0800

    Rename method

--- basic/ch1/webserver_v2.go
+++ basic/ch1/webserver_v2.go
@@ -11,12 +11,12 @@ var mu sync.Mutex
 var count int
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", handlerV2)
 	http.HandleFunc("/count", counter)
 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func handlerV2(w http.ResponseWriter, r *http.Request) {
 	mu.Lock()
 	count++
 	mu.Unlock()
